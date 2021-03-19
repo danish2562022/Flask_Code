30af6bb5c35af597e2959b99bf55dda71afbe476
Danish Anwer <danish2562022@gmail.com>
2021-03-19 23:28:23 +0530

    commit7

--- flask_basic2.py
+++ flask_basic2.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__) # __name__ name of the module
 
-@app.route("/") # Browser url
-def Hello():
-	return "Hello World"
+posts=[
+{
+	'author':'MD Danish Anwer',
+	'title': 'Machine learning blogpost I',
+	'content': ' KNN implementation',
+	'date_posted':'19-March-2021'
+},
+{
+	'author':'MD Danish Anwer',
+	'title':'Machine learning blogpost II',
+	'content': ' Naive Bayes implementation',
+	'date_posted':'19-March-2021'
+}
+]
+
+@app.route('/')
+@app.route("/home") # Browser url
+def home():
+	return render_template('home.html',posts=posts)
+
+@app.route("/about")
+def hello():
+	return render_template('about.html',title='about')
+
+
+if __name__=='__main__':
+	app.run(debug=True)
